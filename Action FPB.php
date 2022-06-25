c4875d6329db67a196788f6c673bb0b5409264c5
LefrandJoySusanto <108160526+LefrandJoySusanto@users.noreply.github.com>
2022-06-25 12:08:10 +0700

    Add files via upload

---
+++ Action FPB.php
@@ -0,0 +1,42 @@
+<?php
+$bil1 = $_POST["Bil1"];
+$bil2 = $_POST["Bil2"];
+
+function FPB(int $b1, int $b2){
+	$b3 = -1;
+	while ($b3 != 0){
+		$b3 = $b1 % $b2;
+		$b1 = $b2;
+		$b2 = $b3;
+	}
+	return $b1;
+}
+
+echo"<p>Bilangan 1 Anda: ", $bil1,"</p>";
+echo"<p>Bilangan 2 Anda: ", $bil2,"</p>";
+
+echo"<p>Langkah pertama: cari faktor dari ",$bil1,"<br>";
+echo"Faktor ",$bil1,": ";  
+for ($a = 1; $a <= $bil1; $a++){
+	if($bil1 % $a == 0){
+		echo $a;
+		if($a != $bil1){
+			echo ", ";
+		}
+	}
+}
+
+echo"<p>Langkah pertama: cari faktor dari ",$bil2,"<br>";
+echo"Faktor ",$bil2,": ";  
+for ($a = 1; $a <= $bil2; $a++){
+	if($bil2 % $a == 0){
+		echo $a;
+		if($a != $bil2){
+			echo ", ";
+		}
+	}
+}
+
+echo"<p>Langkah ketiga: pilih faktor yang sama dan terbesar dari ",$bil1, " dan ",$bil2,"<br>";
+echo"Faktor yang sama dan terbesar adalah ",FPB($bil1, $bil2),"</p>";
+?>
---
+++ FPB.php
@@ -0,0 +1,28 @@
+<?php
+echo"<!DOCTYPE html>";
+echo"<html>";
+echo"<body>";
+
+echo"<p><h1>FPB</h1></p>";
+echo"<p>FPB atau faktor persekutuan terbesar adalah faktor terbesar dari dua atau lebih bilangan bulat yang membagi bilangan bulat tersebut.<br>
+Untuk mencari FPB pertama kita harus mengetahui faktor dari bilangan-bilangan yang dicari FPBnya.<br></p>";
+echo"<p>Sebagai contoh mari kita cari FBP dari bilangan 42 dan 18.</p>";
+echo"<p>Langkah pertama: cari faktor dari 42.<br>
+Faktor 42: 1, 2, 3, 6, 7, 14, 21, 42</p>";
+echo"<p>Langkah kedua: cari faktor dari 18.<br>
+Faktor 18: 1, 2, 3, 6, 9, 18</p>";
+echo"<p>Langkah ketiga: pilih faktor yang sama dan terbesar dari 42 dan 18.<br>
+Faktor yang sama dan terbesar adalah 6.</p>";
+echo"<p>Langkah keempat: ambil kesimpulan.<br>
+Jadi FPB dari 42 dan 18 adalah 6.</p>";
+echo"<p>Silahkan coba dengan angka anda sendiri</p>";
+
+echo"<form action=\"Action FPB.php\" method=\"post\">";
+echo"<p>Bilangan 1: <input type=\"text\" name=\"Bil1\"></p>";
+echo"<p>Bilangan 2: <input type=\"text\" name=\"Bil2\"></p>";
+echo"<input type=\"submit\">";
+echo"</form>";
+
+echo"</body>";
+echo"</html>";
+?>
